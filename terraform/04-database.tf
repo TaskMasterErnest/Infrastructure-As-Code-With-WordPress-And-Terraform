25f9f12939241941d75299bc3278803d6549330d
TaskMasterErnest <kluernest08@gmail.com>
2023-09-06 18:32:41 +0000

    added template for initializing and configuring database

---
+++ terraform/04-database.tf
@@ -0,0 +1,28 @@
+# create the database instance
+# add a password to the database
+# put the database in the required subnets
+resource "aws_db_subnet_group" "database" {
+  name = "${var.name}-${var.environment_type}-db-subnet-group"
+  subnet_ids = [aws_subnet.rds01.id, aws_subnet.rds02.id]
+}
+
+resource "random_password" "database_password" {
+  length = 16
+  special = false
+}
+
+resource "aws_db_instance" "database" {
+  allocated_storage = var.database_allocated_storage
+  db_name = var.database_name
+  engine = var.database_engine
+  engine_version = var.database_engine_version
+  instance_class = var.database_instance_class
+  username = var.database_username
+  password = random_password.database_password.result
+  parameter_group_name = var.database_parameter_group
+  db_subnet_group_name = aws_db_subnet_group.database.name
+  skip_final_snapshot = var.database_skip_final_snapshot
+  vpc_security_group_ids = [aws_security_group.sg_rds.id]
+  
+  tags = merge(var.default_tags, tomap({ Name = "${var.name}-${var.environment_type}-rds" }))
+}
